f67923ac0c1afd75c53c1772e9e33a786b59481b
Ricardo Menck <menckker@gmail.com>
2025-01-24 11:07:29 -0300

    Get lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.GameList;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Objects;
+
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameListDTO that = (GameListDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
